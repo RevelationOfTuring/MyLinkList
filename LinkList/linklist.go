1a9fbd6d6fc2b0af7272e7d2cb7d2ba5f7a219bd
RevelationOfTuring <michael_wangguanyi@163.com>
2019-02-03 12:54:26 +0800

    GetLen,InsertByHeader,InsertByTail,InsertByPos and DeleteByValue

--- LinkList/linklist.go
+++ LinkList/linklist.go
@@ -9,31 +9,120 @@ type LinkNode struct {
 
 type LinkList struct {
 	Header *LinkNode
-	Len int
+	len    int
 }
 
-func (l*LinkList)Create(data ...interface{}){
-	if l==nil {
+func (l *LinkList) Create(data ...interface{}) {
+	if l == nil {
 		return
 	}
 	//assisted pointer
-	pCur:=l.Header
-	lenData:=len(data)
+	pCur := l.Header
+	lenData := len(data)
 
-	for i:=0;i<lenData;i++{
-		newNode:=&LinkNode{data[i],nil}
-		pCur.Next=newNode
-		pCur=newNode
-		l.Len++
+	for i := 0; i < lenData; i++ {
+		newNode := &LinkNode{data[i], nil}
+		pCur.Next = newNode
+		pCur = newNode
+		l.len++
 	}
 }
 
-func (l*LinkList)Print(){
-	if l==nil{
+func (l *LinkList) Print() {
+	if l == nil {
 		return
 	}
 	//头指针里面是不存储任何data的，所以遍历数据最开始应该从header的next开始
-	for i:=l.Header.Next;i!=nil;i=i.Next{
+	for i := l.Header.Next; i != nil; i = i.Next {
 		fmt.Println(i.Data)
+	}
+}
+
+func (l *LinkList) GetLen() int {
+	if l == nil {
+		return -1
+	}
+	return l.len
+}
+
+func (l*LinkList) InsertByHeader(data interface{}){
+	if l==nil {
+		return
+	}
+	if data==nil{
+		return
+	}
+	newNode:=&LinkNode{data,nil}
+	newNode.Next=l.Header.Next
+	l.Header.Next=newNode
+	l.len++
+}
+
+func (l*LinkList) InsertByTail(data interface{}){
+	if l==nil{
+		return
+	}
+	if data==nil{
+		return
+	}
+	pCur:=l.Header
+	for {
+		if pCur.Next!=nil{
+			pCur=pCur.Next
+		}else{
+			newNode:=new(LinkNode)
+			newNode.Data=data
+			pCur.Next=newNode
+			l.len++
+			break
+		}
+	}
+}
+
+func (l*LinkList)InsertByPos(data interface{},pos int){
+	if l==nil{
+		return
+	}
+	if data==nil {
+		return
+	}
+	if pos<=1{
+		l.InsertByHeader(data)
+		return
+	}
+	if pos>l.len{
+		l.InsertByTail(data)
+		return
+	}
+	pCur:=l.Header
+	for i:=1;i<=pos-1;i++{
+		pCur=pCur.Next
+	}
+
+	newNode:=new(LinkNode)
+	newNode.Data=data
+	newNode.Next=pCur.Next
+	pCur.Next=newNode
+	l.len++
+}
+
+func (l*LinkList)DeleteByValue(data interface{}){
+	if l==nil {
+		return
+	}
+	if data==nil{
+		return
+	}
+	pCur:=l.Header
+	var pPre *LinkNode
+	for pCur!=nil {
+		if pCur.Data!=data{
+			pPre=pCur
+			pCur=pCur.Next
+		}else{
+			pPre.Next=pCur.Next
+			l.len--
+			break
+		}
 	}
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"dataStructure/MyLinkList/LinkList"
+	"fmt"
 )
 
 type Student struct {
@@ -16,11 +17,29 @@ func main() {
 	s3:=Student{"C",20,"male"}
 	s4:=Student{"D",21,"male"}
 	s5:=Student{"E",22,"female"}
+	s6:=Student{"WGY",28,"male"}
+	//s7:=Student{"sy",26,"female"}
+
 
 	myLink:=new(LinkList.LinkList)
 	//necessary
 	myLink.Header=new(LinkList.LinkNode)
-	myLink.Create(s1,s2,s3,s4,s5)
+	myLink.Create(s1,s2,s3,s4,s5,s6)
 	myLink.Print()
+	fmt.Println(myLink.GetLen())
+	//myLink.InsertByHeader(s6)
+	//myLink.Print()
+	//fmt.Println(myLink.GetLen())
+	//myLink.InsertByTail(s6)
+	//myLink.Print()
+	//fmt.Println(myLink.GetLen())
+	//myLink.InsertByPos(s6,0)
+	//myLink.Print()
+	//fmt.Println(myLink.GetLen())
+	myLink.DeleteByValue(s2)
+	myLink.Print()
+	fmt.Println(myLink.GetLen())
+
+
 
 }
